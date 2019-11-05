803dd6bda210ca86f71355071966c2f53efaa7f8
Wiljan Vinke <wiljanvinke@gmail.com>
2019-11-05 14:46:46 +0100

    Start with html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tournament-angular';
+  title = 'Tournament Demo in Angular';
 }
