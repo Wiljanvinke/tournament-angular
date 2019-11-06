82f2cd71c6ec12ed53e2ecb20593d98de7c3a501
Wiljan Vinke <wiljanvinke@gmail.com>
2019-11-06 10:28:10 +0100

    Managed to connect to backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Participant } from './participant/participant.component';
 import { ParticipantService } from './participant/participant.service';
@@ -8,15 +8,28 @@ import { ParticipantService } from './participant/participant.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tournament Demo in Angular';
+  test;
   table: HTMLTableElement;
-  participantService: ParticipantService;
+  participants: Participant[];
 
-  public getParticipants(): Observable<Participant[]>{
+  ngOnInit() {
+    this.getParticipants();
+  }
+
+  constructor(public participantService: ParticipantService){}
+
+  public getParticipants(): Observable<Participant[]> {
+    this.table = document.createElement("table");
     let row = this.table.insertRow(-1);
     let cell = row.insertCell(-1);
     cell.innerHTML = this.participantService.getParticipants()[0];
+    this.participantService.getParticipants().subscribe(data => {
+      this.participants = data
+    });
+    console.log(this.participants);
     return this.participantService.getParticipants();
+
   }
 }
